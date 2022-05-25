f6a9eed51345d7e2cd336155d1bd0b328087e8c3
Vladislav Kheynov <lcastory86@gmail.com>
2022-05-25 12:09:14 +0700

    feat: done vectorAdd

--- semester4/kg/lab3/riugaa/vectorAdd.cu
+++ semester4/kg/lab3/riugaa/vectorAdd.cu
@@ -26,68 +26,58 @@
  */
 
 /**
- * Vector addition: C = A + B.
- *
- * This sample is a very basic sample that implements element by element
- * vector addition. It is the same as the sample illustrating Chapter 2
- * of the programming guide with some additions like error checking.
+ * Сложение векторов C = A + B.
  */
 
 #include <stdio.h>
 
-// For the CUDA runtime routines (prefixed with "cuda_")
+// CUDA библиотеки
 #include <cuda_runtime.h>
-
 #include <helper_cuda.h>
+
 /**
- * CUDA Kernel Device code
- *
- * Computes the vector addition of A and B into C. The 3 vectors have the same
- * number of elements numElements.
+ * Вычисляет сумму векторов А и B и записывает в вектор С. Все векторы имеют одинаковое число элементов (numElements)
  */
 __global__ void vectorAdd(const float *A, const float *B, float *C,
                           int numElements) {
-  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  int i = blockDim.x * blockIdx.x + threadIdx.x; //Индекс нити в блоке и сетке блоков
 
   if (i < numElements) {
     C[i] = A[i] + B[i] + 0.0f;
   }
 }
 
-/**
- * Host main routine
- */
 int main(void) {
-  // Error code to check return values for CUDA calls
+  // Код ошибки, которая может возникнуть во время работы
   cudaError_t err = cudaSuccess;
 
-  // Print the vector length to be used, and compute its size
+  // Вывод длины вектора, вычисление размера памяти под этот вектор
   int numElements = 50000;
   size_t size = numElements * sizeof(float);
   printf("[Vector addition of %d elements]\n", numElements);
 
-  // Allocate the host input vector A
+  // Выделяем ОЗУ для вектора А
   float *h_A = (float *)malloc(size);
 
-  // Allocate the host input vector B
+  // Выделяем ОЗУ для вектора В
   float *h_B = (float *)malloc(size);
 
-  // Allocate the host output vector C
+  // Выделяем ОЗУ для выходного вектора С
   float *h_C = (float *)malloc(size);
 
-  // Verify that allocations succeeded
+  // Проверяем что выделение памяти прошло успешно
   if (h_A == NULL || h_B == NULL || h_C == NULL) {
     fprintf(stderr, "Failed to allocate host vectors!\n");
     exit(EXIT_FAILURE);
   }
 
-  // Initialize the host input vectors
+  // Заполняем входные векторы A и B
   for (int i = 0; i < numElements; ++i) {
     h_A[i] = rand() / (float)RAND_MAX;
     h_B[i] = rand() / (float)RAND_MAX;
   }
 
-  // Allocate the device input vector A
+  // Выделяем видеопамять под вектор A
   float *d_A = NULL;
   err = cudaMalloc((void **)&d_A, size);
 
@@ -97,7 +87,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Allocate the device input vector B
+  // Выделяем видеопамять под вектор B
   float *d_B = NULL;
   err = cudaMalloc((void **)&d_B, size);
 
@@ -107,7 +97,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Allocate the device output vector C
+  // Выделяем видеопамять под вектор С
   float *d_C = NULL;
   err = cudaMalloc((void **)&d_C, size);
 
@@ -117,11 +107,9 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Copy the host input vectors A and B in host memory to the device input
-  // vectors in
-  // device memory
+  // Копируем входные векторы A и B в видеопамять
   printf("Copy input data from the host memory to the CUDA device\n");
-  err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+  err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice); //видеопамять под вектор A
 
   if (err != cudaSuccess) {
     fprintf(stderr,
@@ -130,7 +118,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+  err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice); // Видеопамять под вектор В
 
   if (err != cudaSuccess) {
     fprintf(stderr,
@@ -139,22 +127,21 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Launch the Vector Add CUDA Kernel
-  int threadsPerBlock = 256;
-  int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
+  // Запускаем функцию сложения векторов на GPU
+  int threadsPerBlock = 256; //Количество нитей на блок
+  int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock; // Количество блоков в сетке
   printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid,
          threadsPerBlock);
   vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
   err = cudaGetLastError();
 
-  if (err != cudaSuccess) {
+  if (err != cudaSuccess) { //Вывод сообщения об ошибке если она была
     fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n",
             cudaGetErrorString(err));
     exit(EXIT_FAILURE);
   }
 
-  // Copy the device result vector in device memory to the host result vector
-  // in host memory.
+  // Копируем результат сложения векторов из видеопамяти в ОЗУ
   printf("Copy output data from the CUDA device to the host memory\n");
   err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
@@ -165,7 +152,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Verify that the result vector is correct
+  // Проверяем правильность сложения
   for (int i = 0; i < numElements; ++i) {
     if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
       fprintf(stderr, "Result verification failed at element %d!\n", i);
@@ -175,7 +162,7 @@ int main(void) {
 
   printf("Test PASSED\n");
 
-  // Free device global memory
+  // Освобождаем видеопамять для вектора А
   err = cudaFree(d_A);
 
   if (err != cudaSuccess) {
@@ -184,7 +171,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  err = cudaFree(d_B);
+  err = cudaFree(d_B);// Освобождаем видеопамять для вектора В
 
   if (err != cudaSuccess) {
     fprintf(stderr, "Failed to free device vector B (error code %s)!\n",
@@ -200,7 +187,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // Free host memory
+  // Освобождаем память из ОЗУ
   free(h_A);
   free(h_B);
   free(h_C);
