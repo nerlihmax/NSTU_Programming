84d2bb68a0d20aeebd793b86d56d6c4c2e8a67c2
Vladislav Kheynov <lcastory86@gmail.com>
2022-05-29 13:01:14 +0700

    chore

--- semester4/kg/lab3/riugaa/vectorAdd.cu
+++ semester4/kg/lab3/riugaa/vectorAdd.cu
@@ -1,197 +1,435 @@
-/* Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of NVIDIA CORPORATION nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
- * Сложение векторов C = A + B.
- */
-
+// подключение библиотек
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-// CUDA библиотеки
+// OpenGL
+#include <helper_gl.h>
+
+#include <GL/freeglut.h>
+
+// CUDA
 #include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+#include <helper_functions.h>    //  cuda.h, cuda_runtime_api.h
 #include <helper_cuda.h>
 
-/**
- * Вычисляет сумму векторов А и B и записывает в вектор С. Все векторы имеют одинаковое число элементов (numElements)
- */
-__global__ void vectorAdd(const float *A, const float *B, float *C,
-                          int numElements) {
-  int i = blockDim.x * blockIdx.x + threadIdx.x; //Индекс нити в блоке и сетке блоков
+#include <vector_types.h>
 
-  if (i < numElements) {
-    C[i] = A[i] + B[i] + 0.0f;
-  }
+#define MAX_EPSILON_ERROR 10.0f
+#define THRESHOLD          0.30f
+#define REFRESH_DELAY     5 //ms
+
+// константы
+const unsigned int window_width  = 800;
+const unsigned int window_height = 600;
+
+const unsigned int mesh_width    = 256;
+const unsigned int mesh_height   = 256;
+
+// Типы данных для буффера точек
+GLuint vbo;
+struct cudaGraphicsResource *cuda_vbo_resource;
+void *d_vbo_buffer = NULL;
+
+float g_fAnim = 0.0;
+
+// для вращения камеры мышью
+int mouse_old_x, mouse_old_y;
+int mouse_buttons = 0;
+float rotate_x = 0.0, rotate_y = 0.0;
+float translate_z = -3.0;
+
+StopWatchInterface *timer = NULL;
+
+
+int fpsCount = 0;
+int fpsLimit = 1;
+int g_Index = 0;
+float avgFPS = 0.0f;
+unsigned int frameCount = 0;
+unsigned int g_TotalErrors = 0;
+bool g_bQAReadback = false;
+
+int *pArgc = NULL;
+char **pArgv = NULL;
+
+#define MAX(a,b) ((a > b) ? a : b)
+
+bool runTest(int argc, char **argv, char *ref_file);
+void cleanup();
+
+// GL функции
+bool initGL(int *argc, char **argv);
+void createVBO(GLuint *vbo, struct cudaGraphicsResource **vbo_res,
+               unsigned int vbo_res_flags);
+void deleteVBO(GLuint *vbo, struct cudaGraphicsResource *vbo_res);
+
+// callbacks
+void display();
+void keyboard(unsigned char key, int x, int y);
+void mouse(int button, int state, int x, int y);
+void motion(int x, int y);
+void timerEvent(int value);
+
+// Cuda функции
+void runCuda(struct cudaGraphicsResource **vbo_resource);
+void runAutoTest(int devID, char **argv, char *ref_file);
+
+const char *sSDKsample = "RGZ (Paraboloid translation)";
+
+__global__ void simple_vbo_kernel(float4 *pos, unsigned int width, unsigned int height, float time)
+{
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+    // вычисление координат
+    float u = x / (float) width;
+    float v = y / (float) height;
+    u = u*2.0f - 1.0f;
+    v = v*2.0f - 1.0f;
+
+    // вычисление z-координаты в точке
+    float coef = 2.0f;
+    float w = (u*u)/(coef) * sinf(time)*-1.0f + ((v*v)/(coef) * sinf(time));
+
+    //итоговая вершина
+    pos[y*width+x] = make_float4(u, w, v, 1.0f);
 }
 
-int main(void) {
-  // Код ошибки, которая может возникнуть во время работы
-  cudaError_t err = cudaSuccess;
 
-  // Вывод длины вектора, вычисление размера памяти под этот вектор
-  int numElements = 50000;
-  size_t size = numElements * sizeof(float);
-  printf("[Vector addition of %d elements]\n", numElements);
+void launch_kernel(float4 *pos, unsigned int mesh_width,
+                   unsigned int mesh_height, float time)
+{
+    //запуск вычисления
+    dim3 block(8, 8, 1);
+    dim3 grid(mesh_width / block.x, mesh_height / block.y, 1);
+    simple_vbo_kernel<<< grid, block>>>(pos, mesh_width, mesh_height, time);
+}
 
-  // Выделяем ОЗУ для вектора А
-  float *h_A = (float *)malloc(size);
 
-  // Выделяем ОЗУ для вектора В
-  float *h_B = (float *)malloc(size);
+//точка входа в программу
+int main(int argc, char **argv)
+{
+    char *ref_file = NULL;
 
-  // Выделяем ОЗУ для выходного вектора С
-  float *h_C = (float *)malloc(size);
+    pArgc = &argc;
+    pArgv = argv;
 
-  // Проверяем что выделение памяти прошло успешно
-  if (h_A == NULL || h_B == NULL || h_C == NULL) {
-    fprintf(stderr, "Failed to allocate host vectors!\n");
-    exit(EXIT_FAILURE);
-  }
+    setenv ("DISPLAY", ":0", 0);
 
-  // Заполняем входные векторы A и B
-  for (int i = 0; i < numElements; ++i) {
-    h_A[i] = rand() / (float)RAND_MAX;
-    h_B[i] = rand() / (float)RAND_MAX;
-  }
+    printf("%s starting...\n", sSDKsample);
 
-  // Выделяем видеопамять под вектор A
-  float *d_A = NULL;
-  err = cudaMalloc((void **)&d_A, size);
-
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Failed to allocate device vector A (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  // Выделяем видеопамять под вектор B
-  float *d_B = NULL;
-  err = cudaMalloc((void **)&d_B, size);
-
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Failed to allocate device vector B (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  // Выделяем видеопамять под вектор С
-  float *d_C = NULL;
-  err = cudaMalloc((void **)&d_C, size);
-
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Failed to allocate device vector C (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  // Копируем входные векторы A и B в видеопамять
-  printf("Copy input data from the host memory to the CUDA device\n");
-  err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice); //видеопамять под вектор A
-
-  if (err != cudaSuccess) {
-    fprintf(stderr,
-            "Failed to copy vector A from host to device (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice); // Видеопамять под вектор В
-
-  if (err != cudaSuccess) {
-    fprintf(stderr,
-            "Failed to copy vector B from host to device (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  // Запускаем функцию сложения векторов на GPU
-  int threadsPerBlock = 256; //Количество нитей на блок
-  int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock; // Количество блоков в сетке
-  printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid,
-         threadsPerBlock);
-  vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
-  err = cudaGetLastError();
-
-  if (err != cudaSuccess) { //Вывод сообщения об ошибке если она была
-    fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  // Копируем результат сложения векторов из видеопамяти в ОЗУ
-  printf("Copy output data from the CUDA device to the host memory\n");
-  err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
-
-  if (err != cudaSuccess) {
-    fprintf(stderr,
-            "Failed to copy vector C from device to host (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
-
-  // Проверяем правильность сложения
-  for (int i = 0; i < numElements; ++i) {
-    if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
-      fprintf(stderr, "Result verification failed at element %d!\n", i);
-      exit(EXIT_FAILURE);
+    if (argc > 1)
+    {
+        if (checkCmdLineFlag(argc, (const char **)argv, "file"))
+        {
+            getCmdLineArgumentString(argc, (const char **)argv, "file", (char **)&ref_file);
+        }
     }
-  }
 
-  printf("Test PASSED\n");
+    printf("\n");
 
-  // Освобождаем видеопамять для вектора А
-  err = cudaFree(d_A);
+    runTest(argc, argv, ref_file);
 
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Failed to free device vector A (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
+    printf("%s completed, returned %s\n", sSDKsample, (g_TotalErrors == 0) ? "OK" : "ERROR!");
+    exit(g_TotalErrors == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+}
 
-  err = cudaFree(d_B);// Освобождаем видеопамять для вектора В
+void computeFPS()
+{
+    frameCount++;
+    fpsCount++;
 
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Failed to free device vector B (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
+    if (fpsCount == fpsLimit)
+    {
+        avgFPS = 1.f / (sdkGetAverageTimerValue(&timer) / 1000.f);
+        fpsCount = 0;
+        fpsLimit = (int)MAX(avgFPS, 1.f);
 
-  err = cudaFree(d_C);
+        sdkResetTimer(&timer);
+    }
 
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Failed to free device vector C (error code %s)!\n",
-            cudaGetErrorString(err));
-    exit(EXIT_FAILURE);
-  }
+    char fps[256];
+    sprintf(fps, "Cuda GL Interop (VBO): %3.1f fps (Max 100Hz)", avgFPS);
+    glutSetWindowTitle(fps);
+}
 
-  // Освобождаем память из ОЗУ
-  free(h_A);
-  free(h_B);
-  free(h_C);
+// инициализация openGL
+bool initGL(int *argc, char **argv)
+{
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowSize(window_width, window_height);
+    glutCreateWindow("Cuda GL Interop (VBO)");
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMotionFunc(motion);
+    glutTimerFunc(REFRESH_DELAY, timerEvent,0);
 
-  printf("Done\n");
-  return 0;
+    // инициализируем необходимые GL библиотеки
+    if (! isGLVersionSupported(2,0))
+    {
+        fprintf(stderr, "ERROR: Support for necessary OpenGL extensions missing.");
+        fflush(stderr);
+        return false;
+    }
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glDisable(GL_DEPTH_TEST);
+
+    // размер окна
+    glViewport(0, 0, window_width, window_height);
+
+    // проекция(камера)
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLfloat)window_width / (GLfloat) window_height, 0.1, 10.0);
+
+    SDK_CHECK_ERROR_GL();
+
+    return true;
+}
+
+
+bool runTest(int argc, char **argv, char *ref_file)
+{
+    //  Создаём таймер
+    sdkCreateTimer(&timer);
+
+    // выбираем предпочтительный видеоадаптер
+    int devID = findCudaDevice(argc, (const char **)argv);
+
+    if (ref_file != NULL)
+    {
+        // регистрируем в видеопамяти буффер для хранения вершин
+        checkCudaErrors(cudaMalloc((void **)&d_vbo_buffer, mesh_width*mesh_height*4*sizeof(float)));
+
+        // запускаем вычисление на GPU
+        runAutoTest(devID, argv, ref_file);
+
+        cudaFree(d_vbo_buffer); //освобождаем память
+        d_vbo_buffer = NULL;
+    }
+    else
+    {
+        // Проверяем правильно ли инициализировалась GL
+        if (false == initGL(&argc, argv))
+        {
+            return false;
+        }
+
+        // регистрируем колбеки
+        glutDisplayFunc(display);
+        glutKeyboardFunc(keyboard);
+        glutMouseFunc(mouse);
+        glutMotionFunc(motion);
+        glutCloseFunc(cleanup);
+
+        // создаем буффер вершин
+        createVBO(&vbo, &cuda_vbo_resource, cudaGraphicsMapFlagsWriteDiscard);
+
+        // запускаем часть вычислений на CUDA
+        runCuda(&cuda_vbo_resource);
+
+        // запускаем главный цикл рендера
+        glutMainLoop();
+    }
+
+    return true;
+}
+
+
+void runCuda(struct cudaGraphicsResource **vbo_resource)
+{
+    // map OpenGL buffer object for writing from CUDA
+    // привязываем объекты OpenGL буффера для того чтобы CUDA могла записывать в них информацию
+    float4 *dptr;
+    checkCudaErrors(cudaGraphicsMapResources(1, vbo_resource, 0));
+    size_t num_bytes;
+    checkCudaErrors(cudaGraphicsResourceGetMappedPointer((void **)&dptr, &num_bytes,
+                                                         *vbo_resource));
+    launch_kernel(dptr, mesh_width, mesh_height, g_fAnim);
+
+    // отвязываем буффер от CUDA
+    checkCudaErrors(cudaGraphicsUnmapResources(1, vbo_resource, 0));
+}
+
+
+#ifndef FOPEN
+#define FOPEN(fHandle,filename,mode) (fHandle = fopen(filename, mode))
+#endif
+
+
+void sdkDumpBin2(void *data, unsigned int bytes, const char *filename)
+{
+    printf("sdkDumpBin: <%s>\n", filename);
+    FILE *fp;
+    FOPEN(fp, filename, "wb");
+    fwrite(data, bytes, 1, fp);
+    fflush(fp);
+    fclose(fp);
+}
+
+void runAutoTest(int devID, char **argv, char *ref_file)
+{
+    char *reference_file = NULL;
+    void *imageData = malloc(mesh_width*mesh_height*sizeof(float));
+
+    // запуск вычислений
+    launch_kernel((float4 *)d_vbo_buffer, mesh_width, mesh_height, g_fAnim);
+
+    cudaDeviceSynchronize();
+    getLastCudaError("launch_kernel failed");
+
+    checkCudaErrors(cudaMemcpy(imageData, d_vbo_buffer, mesh_width*mesh_height*sizeof(float), cudaMemcpyDeviceToHost));
+
+    sdkDumpBin2(imageData, mesh_width*mesh_height*sizeof(float), "simpleGL.bin");
+    reference_file = sdkFindFilePath(ref_file, argv[0]);
+
+    if (reference_file &&
+        !sdkCompareBin2BinFloat("simpleGL.bin", reference_file,
+                                mesh_width*mesh_height*sizeof(float),
+                                MAX_EPSILON_ERROR, THRESHOLD, pArgv[0]))
+    {
+        g_TotalErrors++;
+    }
+}
+
+// функция создающая буффер точек
+void createVBO(GLuint *vbo, struct cudaGraphicsResource **vbo_res,
+               unsigned int vbo_res_flags)
+{
+    assert(vbo);
+
+    // создание объекта буффера
+    glGenBuffers(1, vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+
+    unsigned int size = mesh_width * mesh_height * 4 * sizeof(float);
+    glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Регистрируем буффер в CUDA
+    checkCudaErrors(cudaGraphicsGLRegisterBuffer(vbo_res, *vbo, vbo_res_flags));
+
+    SDK_CHECK_ERROR_GL();
+}
+
+//удаляем буффер
+void deleteVBO(GLuint *vbo, struct cudaGraphicsResource *vbo_res)
+{
+
+    // unregister this buffer object with CUDA
+    checkCudaErrors(cudaGraphicsUnregisterResource(vbo_res));
+
+    glBindBuffer(1, *vbo);
+    glDeleteBuffers(1, vbo);
+
+    *vbo = 0;
+}
+
+//Колбек вызывающийся при обновлении экрана
+void display()
+{
+    sdkStartTimer(&timer);
+
+    // run CUDA kernel to generate vertex positions
+    // запускаем CUDA ядра чтобы сгенерировать позиции вершин
+    runCuda(&cuda_vbo_resource);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //очищаем дисплей
+
+    // отображаем окно просмотра (камеру)
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0, 0.0, translate_z);
+    glRotatef(rotate_x, 1.0, 0.0, 0.0);
+    glRotatef(rotate_y, 0.0, 1.0, 0.0);
+
+    // рендерим буффер
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexPointer(4, GL_FLOAT, 0, 0);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    glColor3f(1.0, 1.0, 0.0); // делаем поверхность жёлтой
+    glDrawArrays(GL_POINTS, 0, mesh_width * mesh_height);
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    glutSwapBuffers();
+
+    g_fAnim += 0.01f;
+
+    sdkStopTimer(&timer);
+    computeFPS();
+}
+
+void timerEvent(int value)
+{
+    if (glutGetWindow())
+    {
+        glutPostRedisplay();
+        glutTimerFunc(REFRESH_DELAY, timerEvent,0);
+    }
+}
+
+void cleanup()
+{
+    sdkDeleteTimer(&timer);
+
+    if (vbo)
+    {
+        deleteVBO(&vbo, cuda_vbo_resource);
+    }
+}
+
+
+//обработчик нажатий на клавиатуру (клавиша ESC)
+void keyboard(unsigned char key, int /*x*/, int /*y*/)
+{
+    switch (key)
+    {
+        case (27) :
+            glutDestroyWindow(glutGetWindow());
+            return;
+    }
+}
+
+// обработчик событий мыши
+void mouse(int button, int state, int x, int y)
+{
+    if (state == GLUT_DOWN)
+    {
+        mouse_buttons |= 1<<button;
+    }
+    else if (state == GLUT_UP)
+    {
+        mouse_buttons = 0;
+    }
+
+    mouse_old_x = x;
+    mouse_old_y = y;
+}
+
+void motion(int x, int y)
+{
+    float dx, dy;
+    dx = (float)(x - mouse_old_x);
+    dy = (float)(y - mouse_old_y);
+
+    if (mouse_buttons & 1)
+    {
+        rotate_x += dy * 0.2f;
+        rotate_y += dx * 0.2f;
+    }
+    else if (mouse_buttons & 4)
+    {
+        translate_z += dy * 0.01f;
+    }
+
+    mouse_old_x = x;
+    mouse_old_y = y;
 }
