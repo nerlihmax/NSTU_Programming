d6205cdfb0b65400dedf31c696e82aa3ac61f729
Vladislav Kheynov <lcastory86@gmail.com>
2022-09-16 17:40:49 +0700

    refactor

--- Lab1V8/lab1.sql
+++ Lab1V8/lab1.sql
@@ -1,51 +1,51 @@
-drop schema if exists v8 cascade;
-create schema if not exists v8;
+DROP SCHEMA IF EXISTS v8 CASCADE;
+CREATE SCHEMA IF NOT EXISTS v8;
 
-create type v8.payment_state as ENUM ('PAID', 'PARTIALLY PAID', 'UNPAID');
+CREATE TYPE v8.payment_state AS ENUM ('PAID', 'PARTIALLY PAID', 'UNPAID');
 
-create table if not exists v8.cities
+CREATE TABLE IF NOT EXISTS v8.cities
 (
-    id   serial primary key,
-    name text unique not null
+    id   SERIAL PRIMARY KEY,
+    name text UNIQUE NOT NULL
 );
 
-create table if not exists v8.lodgers
+CREATE TABLE IF NOT EXISTS v8.lodgers
 (
-    id          serial primary key,
-    first_name  varchar(30) not null,
-    second_name varchar(30) not null
+    id          serial PRIMARY KEY,
+    first_name  varchar(30) NOT NULL,
+    second_name varchar(30) NOT NULL
 );
 
-create table if not exists v8.repairers
+CREATE TABLE IF NOT EXISTS v8.repairers
 (
-    id            serial primary key,
-    first_name    varchar(30)                       not null,
-    second_name   varchar(30)                       not null,
-    date_of_birth date                              not null,
-    city_id       integer references v8.cities (id) not null
+    id            serial PRIMARY KEY,
+    first_name    varchar(30)                       NOT NULL,
+    second_name   varchar(30)                       NOT NULL,
+    date_of_birth date                              NOT NULL,
+    city_id       integer REFERENCES v8.cities (id) NOT NULL
 );
 
-create table if not exists v8.repairments
+CREATE TABLE IF NOT EXISTS v8.repairments
 (
-    id                  serial primary key,
-    type                text                                 not null,
-    repairer_id         integer references v8.repairers (id) not null,
+    id                  serial PRIMARY KEY,
+    type                text                                 NOT NULL,
+    repairer_id         integer REFERENCES v8.repairers (id) NOT NULL,
     repairment_duration interval
 );
 
-create table if not exists v8.repair_facilities
+CREATE TABLE IF NOT EXISTS v8.repair_facilities
 (
-    id              serial primary key,
-    lodger_id       integer references v8.lodgers (id),
+    id              serial PRIMARY KEY,
+    lodger_id       integer REFERENCES v8.lodgers (id),
     repairment_date date,
-    price           integer not null,
+    price           integer NOT NULL,
     payment_state   v8.payment_state,
-    repairment_id   integer references v8.repairments (id)
+    repairment_id   integer REFERENCES v8.repairments (id)
 );
 
 
-insert into v8.lodgers (first_name, second_name)
-values ('Василий', 'Абобович'),
+INSERT INTO v8.lodgers (first_name, second_name)
+VALUES ('Василий', 'Абобович'),
        ('Ильдар', 'Виноградов'),
        ('Лев', 'Калинин'),
        ('Белла', 'Капустина'),
@@ -55,10 +55,10 @@ values ('Василий', 'Абобович'),
        ('Ника', 'Воронина'),
        ('Карина', 'Ибрагимова'),
        ('Ахмет', 'Логинов')
-on conflict do nothing;
+ON CONFLICT DO NOTHING;
 
-insert into v8.cities(name)
-values ('Нижний Новгород'),
+INSERT INTO v8.cities(name)
+VALUES ('Нижний Новгород'),
        ('Москва'),
        ('Екатиринбург'),
        ('Санкт-Петербург'),
@@ -69,10 +69,10 @@ values ('Нижний Новгород'),
        ('Владивосток'),
        ('Сочи'),
        ('Искитим')
-on conflict do nothing;
+ON CONFLICT DO NOTHING;
 
-insert into v8.repairers (first_name, second_name, date_of_birth, city_id)
-values ('Виктор', 'Ватов', '1955-01-15', 1),
+INSERT INTO v8.repairers (first_name, second_name, date_of_birth, city_id)
+VALUES ('Виктор', 'Ватов', '1955-01-15', 1),
        ('Любовь', 'Муравьёва', '1981-07-28', 2),
        ('Наталья', 'Ларионова', '1962-12-15', 3),
        ('Зоя', 'Измайлова', '1973-02-24', 1),
@@ -82,10 +82,10 @@ values ('Виктор', 'Ватов', '1955-01-15', 1),
        ('Сергей', 'Беляков', '1979-05-18', 5),
        ('Евгений', 'Крюков', '1977-09-11', 8),
        ('Дмитрий', 'Долгоруков', '1999-01-25', 4)
-on conflict do nothing;
+ON CONFLICT DO NOTHING;
 
-insert into v8.repairments (type, repairer_id, repairment_duration)
-values ('Замена окон', 2, '1 week'),
+INSERT INTO v8.repairments (type, repairer_id, repairment_duration)
+VALUES ('Замена окон', 2, '1 week'),
        ('Окрашивание дверей', 1, '1 week'),
        ('Поклеивание обоев', 4, '3 hours'),
        ('Замена ламината', 6, '2 weeks'),
@@ -95,10 +95,10 @@ values ('Замена окон', 2, '1 week'),
        ('Сборка шкафа-купе', 5, '5 hours'),
        ('Ремонт сантехники', 9, '1 hour'),
        ('Замена замка на входной двери', 1, '3 hours')
-on conflict do nothing;
+ON CONFLICT DO NOTHING;
 
-insert into v8.repair_facilities (lodger_id, repairment_date, price, payment_state, repairment_id)
-values (1, '2020-01-22', 5000, 'PAID', 1),
+INSERT INTO v8.repair_facilities (lodger_id, repairment_date, price, payment_state, repairment_id)
+VALUES (1, '2020-01-22', 5000, 'PAID', 1),
        (2, '2021-02-23', 6000, 'PAID', 2),
        (4, '2017-03-12', 10000, 'PARTIALLY PAID', 3),
        (5, '2018-04-01', 2000, 'PAID', 4),
@@ -108,19 +108,22 @@ values (1, '2020-01-22', 5000, 'PAID', 1),
        (7, '2022-03-06', 15000, 'PAID', 8),
        (10, '2020-09-08', 1500, 'PARTIALLY PAID', 9),
        (2, '2012-11-22', 5000, 'PAID', 10)
-on conflict do nothing;
+ON CONFLICT DO NOTHING;
 
 
-create or replace view v8.repairment_info as
-select lodger.second_name             as lodger_surname,
-       repairment.type                as repairment_type,
-       repairer.second_name           as repairer_surname,
-       city.name                      as city,
-       repairment.repairment_duration as repairment_duration,
-       facility.repairment_date       as repairment_date,
-       facility.price                 as price
-from v8.repair_facilities as facility
-         inner join v8.lodgers as lodger on lodger.id = facility.lodger_id
-         inner join v8.repairments as repairment on repairment.id = facility.repairment_id
-         inner join v8.repairers as repairer on repairer.id = repairment.repairer_id
-         inner join v8.cities as city on city.id = repairer.city_id;
+CREATE OR REPLACE VIEW v8.repairment_info AS
+SELECT lodger.second_name             AS lodger_surname,
+       repairment.type                AS repairment_type,
+       repairer.second_name           AS repairer_surname,
+       city.name                      AS city,
+       repairment.repairment_duration AS repairment_duration,
+       facility.repairment_date       AS repairment_date,
+       facility.price                 AS price
+FROM v8.repair_facilities AS facility
+         INNER JOIN v8.lodgers AS lodger ON lodger.id = facility.lodger_id
+         INNER JOIN v8.repairments AS repairment ON repairment.id = facility.repairment_id
+         INNER JOIN v8.repairers AS repairer ON repairer.id = repairment.repairer_id
+         INNER JOIN v8.cities AS city ON city.id = repairer.city_id;
+
+SELECT *
+FROM v8.repairment_info;
