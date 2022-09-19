8c9e51bf29f614358f9bb24fc87ae00de5a1a414
Vladislav Kheynov <lcastory86@gmail.com>
2022-09-19 15:57:10 +0700

    feat(lab_2_v5): add schema and data

---
+++ Lab2V5/lab2.sql
@@ -0,0 +1,71 @@
+drop database if exists lab2;
+create database lab2;
+\c lab2;
+
+drop role if exists lab2_user;
+create role lab2_user with login password '7P2bv6QIKEc';
+alter database lab2 owner to lab2_user;
+
+create type steering_side as enum ('LEFT', 'RIGHT');
+
+create type type_of_drive as enum ('AWD', 'FWD', 'RWD');
+
+create type body_type as enum ('Sedan', 'Wagon', 'Minibus');
+
+create table if not exists characteristics
+(
+    id                  serial primary key,
+    steering_side       steering_side not null,
+    transmission        text          not null,
+    fuel_injection_type text          not null,
+    fuel_type           text          not null,
+    type_of_drive       type_of_drive not null
+);
+
+create table if not exists brand
+(
+    id        serial primary key,
+    name      text      not null,
+    body_type body_type not null
+);
+
+create table if not exists car
+(
+    id                 serial primary key,
+    name               text                                        not null,
+    date_of_issue      date check ( date_of_issue < current_date ) not null,
+    price              integer                                     not null,
+    car_brand_id       integer references brand (id)               not null,
+    characteristics_id integer references characteristics (id)     not null
+);
+
+-- Incorrect
+insert into characteristics(steering_side, transmission, fuel_injection_type, fuel_type, type_of_drive)
+values ('Right', 'Automatic', 'TSI', 'Gasoline', 'FWD');
+
+insert into characteristics(steering_side, transmission, fuel_injection_type, fuel_type, type_of_drive)
+values ('LEFT', 'Manual', 'TSI', 'Diesel', 'All Wheels Drive');
+
+-- Correct
+insert into characteristics(steering_side, transmission, fuel_injection_type, fuel_type, type_of_drive)
+values ('RIGHT', 'Automatic', 'TSI', 'Gasoline', 'FWD');
+
+insert into characteristics(steering_side, transmission, fuel_injection_type, fuel_type, type_of_drive)
+values ('LEFT', 'Manual', 'TSI', 'Diesel', 'AWD');
+
+
+-- Incorrect
+insert into brand (name, body_type)
+values ('Toyota', 'Hatchback');
+
+-- Correct
+insert into brand (name, body_type)
+values ('Toyota', 'Sedan');
+
+-- Correct
+insert into car (name, date_of_issue, price, car_brand_id, characteristics_id)
+values ('Vista', '10-12-2020', 4000, 1, 1);
+
+-- Incorrect
+insert into car (name, date_of_issue, price, car_brand_id, characteristics_id)
+values ('Hilux', '22-12-2023', 400000, 1, 2);
