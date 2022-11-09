ceb2d461109d7334568b2d667046735b48184ffc
Vladislav Kheynov <lcastory86@gmail.com>
2022-11-09 19:26:28 +0700

    feat(db): add cw queries

--- semester5/db/Course Work/cw_analytics.sql
+++ semester5/db/Course Work/cw_analytics.sql
@@ -41,7 +41,8 @@ create or replace function products_price_between(a integer, b integer)
                 provider      text,
                 price         integer,
                 country       text,
-                date_of_sale  date
+                date_of_sale  date,
+                is_defect     boolean
             )
 as
 $$
@@ -56,7 +57,8 @@ begin
                         product_info.provider,
                         product_info.price,
                         product_info.country,
-                        product_info.date_of_sale
+                        product_info.date_of_sale,
+                        product_info.is_defect
                  from product_info
                  where product_info.price between a and b;
 end;
@@ -74,7 +76,8 @@ create or replace function products_specific_provider(provider_id integer)
                 provider      text,
                 price         integer,
                 country       text,
-                date_of_sale  date
+                date_of_sale  date,
+                is_defect     boolean
             )
 as
 $$
@@ -84,7 +87,8 @@ begin
                         product_info.provider,
                         product_info.price,
                         product_info.country,
-                        product_info.date_of_sale
+                        product_info.date_of_sale,
+                        product_info.is_defect
                  from product_info
                           inner join providers p on product_info.provider = p.name
                  where p.id = provider_id;
@@ -127,7 +131,8 @@ create or replace function products_with_specific_date(_date date)
                 provider      text,
                 price         integer,
                 country       text,
-                date_of_sale  date
+                date_of_sale  date,
+                is_defect     boolean
             )
 as
 $$
@@ -137,7 +142,8 @@ begin
                         product_info.provider,
                         product_info.price,
                         product_info.country,
-                        product_info.date_of_sale
+                        product_info.date_of_sale,
+                        product_info.is_defect
                  from product_info
                  where product_info.date_of_issue = _date;
 end;
@@ -155,7 +161,8 @@ create or replace function products_with_date_interval(_date daterange)
                 provider      text,
                 price         integer,
                 country       text,
-                date_of_sale  date
+                date_of_sale  date,
+                is_defect     boolean
             )
 as
 $$
@@ -165,7 +172,8 @@ begin
                         product_info.provider,
                         product_info.price,
                         product_info.country,
-                        product_info.date_of_sale
+                        product_info.date_of_sale,
+                        product_info.is_defect
                  from product_info
                  where _date @> product_info.date_of_sale;
 end;
@@ -197,8 +205,38 @@ begin
     return round((sold_during_period::double precision / all_products::double precision) * 100);
 end;
 $$ language plpgsql;
+create or replace function products_sold_during_period(_date daterange, _provider_id integer)
+    returns integer
+as
+$$
+declare
+    sold_during_period integer;
+    all_products       integer;
+begin
+    select count(product_info.name)
+    into all_products
+    from product_info
+             inner join providers p on product_info.provider = p.name
+    where date_of_sale is not null
+      and p.id = _provider_id;
 
+    select count(product_info.name)
+    into sold_during_period
+    from product_info
+             inner join providers p on product_info.provider = p.name
+    where _date @> product_info.date_of_sale
+      and date_of_sale is not null
+      and p.id = _provider_id;
+
+    return round((sold_during_period::double precision / all_products::double precision) * 100);
+end;
+$$ language plpgsql;
+
+--Products sold during period
 select products_sold_during_period('[2021-01-01, 2021-12-31]'::daterange) || '%' as percentage;
+
+--Products sold during period with provider
+select products_sold_during_period('[2021-01-01, 2021-12-31]'::daterange, 1) || '%' as percentage;
 
 create or replace function defected_products_from_country_and_provider(_country_id integer, provider_id integer)
     returns integer
@@ -219,4 +257,69 @@ begin
 end;
 $$ language plpgsql;
 
+--Defected products from country with provider
 select defected_products_from_country_and_provider(10, 1);
+
+create or replace function products_by_provider_more_expensive_than_avg(_country_id integer, provider_id integer)
+    returns table
+            (
+                name          text,
+                date_of_issue date,
+                provider      text,
+                price         integer,
+                country       text,
+                date_of_sale  date,
+                is_defect     boolean
+            )
+as
+$$
+begin
+    return query select product_info.name,
+                        product_info.date_of_issue,
+                        product_info.provider,
+                        product_info.price,
+                        product_info.country,
+                        product_info.date_of_sale,
+                        product_info.is_defect
+                 from product_info
+                          inner join providers p on product_info.provider = p.name
+                 where p.id = provider_id
+                   and product_info.price > (select avg(product_info.price)
+                                             from product_info
+                                                      inner join countries c on product_info.country = c.name
+                                             where c.id = _country_id);
+end;
+$$ language plpgsql;
+
+--Products by provider more expensive than average price for products from country
+select *
+from products_by_provider_more_expensive_than_avg(1, 1);
+
+create or replace function products_cheaper_than_specified(_price integer, _provider_id integer) returns integer
+as
+$$
+declare
+    all_products integer;
+    cheapest     integer;
+begin
+    select count(product_info.name)
+    into all_products
+    from product_info
+             inner join providers p on product_info.provider = p.name
+    where date_of_sale is null
+      and p.id = _provider_id;
+
+    select count(product_info.name)
+    into cheapest
+    from product_info
+             inner join providers p on product_info.provider = p.name
+    where product_info.price < _price
+      and date_of_sale is null
+      and p.id = _provider_id;
+    return round((cheapest::double precision / all_products::double precision) * 100);
+end;
+$$ language plpgsql;
+
+--Products cheaper than specified with specified provider
+select products_cheaper_than_specified(10000, 4) || '%' as percentage;
+
