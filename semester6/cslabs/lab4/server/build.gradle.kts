c260e8bf7dd64adaf19f7ffe88b3b81677da6fc2
Vladislav Kheynov <lcastory86@gmail.com>
2023-03-29 00:52:27 +0700

    feat(cslabs|lab4): server side done

--- semester6/cslabs/lab4/server/build.gradle.kts
+++ semester6/cslabs/lab4/server/build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation("com.sparkjava:spark-core:2.9.4")
     implementation("com.sparkjava:spark-kotlin:1.0.0-alpha")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 }
 
 tasks.test {
--- semester6/cslabs/lab4/server/src/main/kotlin/Main.kt
+++ semester6/cslabs/lab4/server/src/main/kotlin/Main.kt
@@ -1,22 +1,119 @@
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.eclipse.jetty.http.HttpStatus
+import spark.Spark.path
+import spark.kotlin.delete
 import spark.kotlin.get
+import spark.kotlin.post
+import java.io.File
 
 fun main() {
+    ObjectsContainer.restoreFromFile()
     get("/objects") {
         response.type("application/json")
-        ObjectsContainer.getObjects()
+        Json.encodeToString(ObjectsContainer.getObjects())
     }
+    path("/object") {
+        get("/get") {
+            val id = queryParams("id").run { if (isBlank()) return@get "Id is empty" else toInt() }
+            response.type("application/json")
+            Json.run {
+                encodeToString(ObjectsContainer.getObject(id) ?: kotlin.run {
+                        response.type("plain/text")
+                        response.status(HttpStatus.NOT_FOUND_404)
+                        return@get ""
+                    })
+            }
+        }
+        post("/add") {
+            try {
+                val obj = Json.decodeFromString<GraphicalObject>(request.body())
+                if (ObjectsContainer.addObject(obj)) {
+                    response.status(HttpStatus.OK_200)
+                    return@post ""
+                }
+                response.status(HttpStatus.CONFLICT_409)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                response.status(HttpStatus.BAD_REQUEST_400)
+            }
+            return@post ""
+        }
+        delete("/remove") {
+            val id = queryParams("id").run { if (isBlank()) return@delete "Id is empty" else toInt() }
+            if (!ObjectsContainer.deleteObject(id)) {
+                response.status(HttpStatus.NOT_FOUND_404)
+            }
+            return@delete ""
+        }
+    }
+
+    Runtime.getRuntime().addShutdownHook(Thread {
+        ObjectsContainer.saveToFile()
+    })
 }
 
 @Serializable
-data class GraphicalObject(val type: String)
+sealed interface GraphicalObject {
+    @Serializable
+    @SerialName("star")
+    data class Star(
+        val x: Int,
+        val y: Int,
+        val width: Int,
+        val height: Int,
+        val r: Int,
+        val g: Int,
+        val b: Int,
+        val numberOfVertices: Int,
+    ) : GraphicalObject
 
-sealed interface Object {
-    data class Star()
+    @Serializable
+    @SerialName("smiley")
+    data class Smiley(
+        val x: Int,
+        val y: Int,
+        val width: Int,
+        val height: Int,
+        val r: Int,
+        val g: Int,
+        val b: Int,
+        val vx: Int,
+        val vy: Int,
+    ) : GraphicalObject
 }
 
 object ObjectsContainer {
     private val objects = mutableListOf<GraphicalObject>()
+    fun getObjects(): List<GraphicalObject> = objects.toList()
+    fun addObject(obj: GraphicalObject) = objects.add(obj)
+    fun getObject(idx: Int) = objects.getOrNull(idx)
+    fun deleteObject(idx: Int): Boolean {
+        return try {
+            objects.removeAt(idx)
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
 
-    fun getObjects(): List<GraphicalObject> = objects
+    fun saveToFile() {
+        File("./resources/backup.json").printWriter().use { out ->
+            Json.encodeToString(objects).let { out.write(it) }
+        }
+    }
+
+    fun restoreFromFile() {
+        File("./resources/backup.json").bufferedReader().use { reader ->
+            try {
+                Json.decodeFromString<List<GraphicalObject>>(reader.readText()).let { objects.addAll(it) }
+            } catch (e: Exception) {
+                println("Nothing to restore")
+            }
+        }
+    }
 }
