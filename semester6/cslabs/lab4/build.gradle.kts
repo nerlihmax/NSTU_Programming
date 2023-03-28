ca11ff41eab7aa94470075928fb77d985a4e6dc3
Vladislav Kheynov <lcastory86@gmail.com>
2023-03-28 13:23:38 +0700

    setup(lab4): initial commit
    chore(lab4): ignore .gradle

---
+++ semester6/cslabs/lab4/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+}
+
+group = "ru.kheynov"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.sparkjava:spark-core:2.9.4")
+    implementation("com.sparkjava:spark-kotlin:1.0.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
