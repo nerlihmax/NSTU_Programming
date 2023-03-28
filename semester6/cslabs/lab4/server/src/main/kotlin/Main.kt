d160147a75bd1c39b65fee265c03d3726eef844d
Vladislav Kheynov <lcastory86@gmail.com>
2023-03-29 01:09:33 +0700

    chore(islabs|lab4): add comments to code

--- semester6/cslabs/lab4/server/src/main/kotlin/Main.kt
+++ semester6/cslabs/lab4/server/src/main/kotlin/Main.kt
@@ -11,17 +11,22 @@ import spark.kotlin.post
 import java.io.File
 
 fun main() {
-    ObjectsContainer.restoreFromFile()
+    ObjectsContainer.restoreFromFile() // восстановление объектов из файла
+
     get("/objects") {
         response.type("application/json")
-        Json.encodeToString(ObjectsContainer.getObjects())
+        Json.encodeToString(ObjectsContainer.getObjects()) // Получение всех объектов
     }
     path("/object") {
         get("/get") {
-            val id = queryParams("id").run { if (isBlank()) return@get "Id is empty" else toInt() }
+            val id =
+                queryParams("id").run { if (isBlank()) return@get "Id is empty" else toInt() }
+            // получение id объекта из параметров запроса и проверка на пустоту
+
             response.type("application/json")
             Json.run {
-                encodeToString(ObjectsContainer.getObject(id) ?: kotlin.run {
+                encodeToString(ObjectsContainer.getObject(id) // получение объекта по id
+                    ?: kotlin.run { // если объекта с таким id нет, то возвращается пустая строка и код 404
                         response.type("plain/text")
                         response.status(HttpStatus.NOT_FOUND_404)
                         return@get ""
@@ -30,37 +35,37 @@ fun main() {
         }
         post("/add") {
             try {
-                val obj = Json.decodeFromString<GraphicalObject>(request.body())
-                if (ObjectsContainer.addObject(obj)) {
+                val obj = Json.decodeFromString<GraphicalObject>(request.body()) // десериализация объекта из тела запроса
+                if (ObjectsContainer.addObject(obj)) { // добавление объекта в список
                     response.status(HttpStatus.OK_200)
                     return@post ""
                 }
-                response.status(HttpStatus.CONFLICT_409)
+                response.status(HttpStatus.CONFLICT_409) // если объект добавить не удалось, то возвращается код 409
             } catch (e: Exception) {
                 e.printStackTrace()
-                response.status(HttpStatus.BAD_REQUEST_400)
+                response.status(HttpStatus.BAD_REQUEST_400) // если объект не удалось десериализовать, то возвращается код 400
             }
             return@post ""
         }
         delete("/remove") {
             val id = queryParams("id").run { if (isBlank()) return@delete "Id is empty" else toInt() }
-            if (!ObjectsContainer.deleteObject(id)) {
-                response.status(HttpStatus.NOT_FOUND_404)
+            if (!ObjectsContainer.deleteObject(id)) { // удаление объекта по id
+                response.status(HttpStatus.NOT_FOUND_404) // если объекта с таким id нет, то возвращается код 404
             }
             return@delete ""
         }
     }
 
-    Runtime.getRuntime().addShutdownHook(Thread {
+    Runtime.getRuntime().addShutdownHook(Thread { // сохранение объектов в файл при завершении программы
         ObjectsContainer.saveToFile()
     })
 }
 
-@Serializable
-sealed interface GraphicalObject {
+@Serializable // аннотация для сериализации в json
+sealed interface GraphicalObject { // интерфейс графического объекта
     @Serializable
     @SerialName("star")
-    data class Star(
+    data class Star( // звезда
         val x: Int,
         val y: Int,
         val width: Int,
@@ -73,7 +78,7 @@ sealed interface GraphicalObject {
 
     @Serializable
     @SerialName("smiley")
-    data class Smiley(
+    data class Smiley( // смайлик
         val x: Int,
         val y: Int,
         val width: Int,
@@ -87,11 +92,12 @@ sealed interface GraphicalObject {
 }
 
 object ObjectsContainer {
-    private val objects = mutableListOf<GraphicalObject>()
-    fun getObjects(): List<GraphicalObject> = objects.toList()
-    fun addObject(obj: GraphicalObject) = objects.add(obj)
-    fun getObject(idx: Int) = objects.getOrNull(idx)
-    fun deleteObject(idx: Int): Boolean {
+    private val objects = mutableListOf<GraphicalObject>() // список объектов
+
+    fun getObjects(): List<GraphicalObject> = objects.toList() // получение списка объектов
+    fun addObject(obj: GraphicalObject) = objects.add(obj) // добавление объекта
+    fun getObject(idx: Int) = objects.getOrNull(idx) // получение объекта по индексу
+    fun deleteObject(idx: Int): Boolean { // удаление объекта по индексу
         return try {
             objects.removeAt(idx)
             true
@@ -101,18 +107,19 @@ object ObjectsContainer {
         }
     }
 
-    fun saveToFile() {
-        File("./resources/backup.json").printWriter().use { out ->
-            Json.encodeToString(objects).let { out.write(it) }
+    fun saveToFile() { // сохранение объектов в файл
+        File("./resources/backup.json").printWriter().use { out -> // открытие файла
+            Json.encodeToString(objects).let { out.write(it) } // сериализация списка объектов в json и запись в файл
         }
     }
 
-    fun restoreFromFile() {
-        File("./resources/backup.json").bufferedReader().use { reader ->
+    fun restoreFromFile() { // восстановление объектов из файла
+        File("./resources/backup.json").bufferedReader().use { reader -> // открытие файла
             try {
                 Json.decodeFromString<List<GraphicalObject>>(reader.readText()).let { objects.addAll(it) }
+            // десериализация списка объектов из json и добавление в список
             } catch (e: Exception) {
-                println("Nothing to restore")
+                println("Nothing to restore") // если файл пустой, то ничего не делаем
             }
         }
     }
